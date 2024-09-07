70c19d6313365caf71a3176a8134e4dd4841260e
Ning Yu Luo <sslnyx@gmail.com>
2024-09-07 12:36:13 -0700

    update app name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Render!');
+    res.send('Hello, Render! Stripe');
 });
 
 app.listen(PORT, () => {
